ec39fb932e92df18c5202392235348ae80a718cf
syafiranurullita <syafirnurullita3107@gmail.com>
2022-07-18 19:58:09 +0700

    tugas 21 js
    untuk niomic

---
+++ Tugas21/Tugas21.js
@@ -0,0 +1,7 @@
+function isi() {
+  var cari = "Saya belajar masak nasi goreng"
+  console.log(cari);
+  console.log(cari.replace("masak","makan"));
+}
+
+isi(); //untuk mengganti kalimat kata yang diinginkan
